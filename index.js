3284d47a3b4b31b362177bf127e6ecbfd30f8dae
Mark Tobler <marktobler@gmail.com>
2019-11-16 15:04:02 -0800

    still working with the USPS api, but everything else is decent

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const cool = require('cool-ascii-faces');
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+var usps = require('usps-web-tools-node-sdk');
 const PORT = process.env.PORT || 5000
+
+// USPS API configure
+usps.configure({ userID: '711BYUID0181' });
+
 //PG database
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -9,17 +14,25 @@ const pool = new Pool({
   ssl: true
 });
 
+// much of this is from the Heroku getting started Node template.
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  // for parsing application/x-www-form-urlencoded
+  .use(express.urlencoded({ extended: true }))
+  // .use(express.static('public'))
+  // defining the views path. this is required for templates.
   .set('views', path.join(__dirname, 'views'))
+  // setting view engine to ejs. If pug, then we'd just replace ejs with pug
+  // this is also required for templates.
   .set('view engine', 'ejs')
+  // and all these get paths are in the views/ directory
   .get('/', (req, res) => res.render('pages/index'))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      const results = { 'results': (result) ? result.rows : null };
+      res.render('pages/db', results);
       client.release();
     } catch (err) {
       console.error(err);
@@ -28,8 +41,16 @@ express()
   })
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  //.post('/calc', (req, res) => res.send(calculateShipping(req)))
+  //.post('/calc', (req, res) => res.send(calculateRate(req.body.type, req.body.weight)))
+  .post('/calc', (req, res) => {
+    var total = calculateRate(req.body.type, req.body.weight);
+    res.render('pages/getRate', { title: "Postage Calculator", content: "CS313 Week10 Prove: Node, Express, EJS, and You", total: total, error: ''});
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+
+// functions
 showTimes = () => {
   let result = '';
   const times = process.env.TIMES || 5;
@@ -37,4 +58,74 @@ showTimes = () => {
     result += i + ' ';
   }
   return result;
+}
+
+calculateRate = (type, weight) => {
+  var base = 0;
+  var total = 0;
+  console.log(type + ', ' + weight);
+  switch (type) {
+    case 'S':
+      base = .55;
+      total = (base + (.15 * (weight > 3 ? 3 : Math.trunc(weight) - 1)));
+      break;
+    case 'M':
+      base = .50;
+      total = (base + (.15 * (weight > 3 ? 3 : Math.trunc(weight) - 1)));
+      break;
+    case 'F':
+      base = 1;
+      total = (base + (.15 * (weight > 13 ? 13 : Math.trunc(weight) - 1)));
+      break;
+    case '1':
+      // I don't want to do a nested switch. To make this meaningful
+      // I'm going to play around with the USPS API.  
+      //total = 3.66;
+      // need to make this wait? 
+      total = uspsGetRate(weight);
+      total = total == NaN ? 3.66 : total;
+      console.log(total);
+      // arg!
+      total = 3.66;
+      break;
+  }
+
+  return (total.toFixed(2));
+}
+
+
+
+// still playing around with this
+// have to make it wait I think.
+async function uspsGetRate(weight) {
+  usps.rateCalculator.rate(
+    { // build a USPS package object, passing in weight
+      // ran out of time, was going to replace the zip's with actual
+      // form input. Arg. As it is, I need to pull out the rate from the json.
+      // not sure how to do that in Node. Will need to investigate more. =()
+      revision: '2',
+      package: [
+        {
+          service: 'FIRST CLASS',
+          firstClassMailType: 'LETTER',
+          zipOrigination: '89117',
+          zipDestination: '83460',
+          pounds: '0',
+          ounces: weight,
+          size: 'REGULAR',
+          machinable: true
+        }
+      ]
+    }, function (error, response) {
+      if (error) {
+        console.log(error);
+        return Number.parseFloat('0');
+      } else {
+        console.log(JSON.stringify(response));
+        // need to dip into the response body and pull out the rate. ><
+        //return JSON.stringify(response);
+        return Number.parseFloat('3.66');
+      }
+    }
+  );
 }
